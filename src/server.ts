7ea79b22ae54c05e96bc84ae6e28ab376ef8cac2
Antonio <acgomesn@gmail.com>
2023-04-04 21:46:31 -0300

    conectando banco de dados sqlite3

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 // GET, POST, PUT, PATCH, DELETE
 
-app.get('/hello', () => {
-  return 'Hello world'
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
