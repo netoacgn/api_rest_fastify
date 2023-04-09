5d5e5138fa3e200f4129449d50b274c4b9be8db2
Antonio <acgomesn@gmail.com>
2023-04-09 12:28:56 -0300

    tratando env com zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { knex } from './database'
 import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
@@ -16,7 +17,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server listening on port 3333')
